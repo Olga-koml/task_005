2cea7c9c418b472aa95326d0977063a1dc95c9d6
Olga Komleva <olyakomleva@mail.ru>
2024-03-12 10:15:58 +0300

    task 1 done try workflow2

--- src/house.c
+++ src/house.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 #include "house_calc.h"
 
 int main() {
     int width, length, height, budget;
     if (scanf("%d%d%d%d", &width, &length, &height, &budget) &&
-        width > 0 && length > 0 && height > 0) {
+    width > 0 && length > 0 && height > 0) {
         calculate_house_cost(width, length, height, budget);
-        } else print_error();
+        } else
+        print_error();
     return 0;
 }
