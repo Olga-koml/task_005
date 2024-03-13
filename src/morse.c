8627dfa7ce8368e56d060772d8bed02400b8d348
Olga Komleva <olyakomleva@mail.ru>
2024-03-14 00:39:52 +0300

    task 3 try 12

--- src/morse.c
+++ src/morse.c
@@ -3,17 +3,18 @@
 #include "morse_utils.h"
 
 int main() {
-    char str[250];
     int command;
     char c;
     if (scanf("%d%c", &command, &c) && c == '\n') {
         switch (command) {
             case 1: {
+                char str[250];
                 fgets(str, sizeof(str), stdin);
                 encode_to_morse_and_print(str);
                 break;
             }
             case 2: {
+                char str[250];
                 fgets(str, sizeof(str), stdin);
                 decode_from_morse(str);
                 break;
