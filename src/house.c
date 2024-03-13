f060e58aa987658aca259400ffb6f1a0ea35c99f
Olga Komleva <olyakomleva@mail.ru>
2024-03-13 23:56:39 +0300

    task 3 try 4

--- src/house.c
+++ src/house.c
@@ -2,12 +2,13 @@
 
 #include "house_calc.h"
 
+
 int main() {
     int width, length, height, budget;
     if (scanf("%d%d%d%d", &width, &length, &height, &budget) && width > 0 && length > 0 && height > 0) {
         calculate_house_cost(width, length, height, budget);
-    } else {
+    } else
         print_error();
-    }
+
     return 0;
 }
